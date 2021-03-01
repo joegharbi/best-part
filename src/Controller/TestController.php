1209ac5f622ac8478ea7fc46ec55e72f7a6f5459
Youssef Gharbi <joe.gharbi@gmail.com>
2021-03-01 15:06:28 +0100

    first commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+
+    /**
+     *
+     * @Route("/index",name="index", methods={"GET", "POST"},host="localhost",
+     * schemes={"http",https"})
+     */
+
+    public function index()
+    {
+        dd("Test index");
+    }
+
+    /**
+     *
+     * @Route("/test/{age<\d+>?0},name="test", methods={"GET", "POST"},host="localhost",
+     * schemes={"http",https"})
+     */
+
+    public function test(Request $request, $age)
+    {
+        return dd($request);
+        return dd("Vous avez $age ans!");
+    }
+}
