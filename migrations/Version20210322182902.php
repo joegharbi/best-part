06ec5fd938328d4de93376e376c9fce565623ef8
Youssef Gharbi <joe.gharbi@gmail.com>
2021-04-01 17:55:06 +0200

    more fixes.

--- migrations/Version20210322182902.php
+++ migrations/Version20210322182902.php
@@ -24,15 +24,15 @@ final class Version20210322182902 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE model_production_year (model_id INT NOT NULL, production_year_id INT NOT NULL, INDEX IDX_4FBD36617975B7E7 (model_id), INDEX IDX_4FBD366155D4737A (production_year_id), PRIMARY KEY(model_id, production_year_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE model_production_year ADD CONSTRAINT FK_4FBD36617975B7E7 FOREIGN KEY (model_id) REFERENCES model (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE model_production_year ADD CONSTRAINT FK_4FBD366155D4737A FOREIGN KEY (production_year_id) REFERENCES production_year (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE model ADD make_id INT NOT NULL');
-        $this->addSql('ALTER TABLE model ADD CONSTRAINT FK_D79572D9CFBF73EB FOREIGN KEY (make_id) REFERENCES make (id)');
-        $this->addSql('CREATE INDEX IDX_D79572D9CFBF73EB ON model (make_id)');
-        $this->addSql('ALTER TABLE product ADD model_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD7975B7E7 FOREIGN KEY (model_id) REFERENCES model (id)');
-        $this->addSql('CREATE INDEX IDX_D34A04AD7975B7E7 ON product (model_id)');
+//        $this->addSql('CREATE TABLE model_production_year (model_id INT NOT NULL, production_year_id INT NOT NULL, INDEX IDX_4FBD36617975B7E7 (model_id), INDEX IDX_4FBD366155D4737A (production_year_id), PRIMARY KEY(model_id, production_year_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+//        $this->addSql('ALTER TABLE model_production_year ADD CONSTRAINT FK_4FBD36617975B7E7 FOREIGN KEY (model_id) REFERENCES model (id) ON DELETE CASCADE');
+//        $this->addSql('ALTER TABLE model_production_year ADD CONSTRAINT FK_4FBD366155D4737A FOREIGN KEY (production_year_id) REFERENCES production_year (id) ON DELETE CASCADE');
+//        $this->addSql('ALTER TABLE model ADD make_id INT NOT NULL');
+//        $this->addSql('ALTER TABLE model ADD CONSTRAINT FK_D79572D9CFBF73EB FOREIGN KEY (make_id) REFERENCES make (id)');
+//        $this->addSql('CREATE INDEX IDX_D79572D9CFBF73EB ON model (make_id)');
+//        $this->addSql('ALTER TABLE product ADD model_id INT DEFAULT NULL');
+//        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD7975B7E7 FOREIGN KEY (model_id) REFERENCES model (id)');
+//        $this->addSql('CREATE INDEX IDX_D34A04AD7975B7E7 ON product (model_id)');
     }
 
     public function down(Schema $schema) : void
